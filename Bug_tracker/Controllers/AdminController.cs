394bd8cdf6a7d34ab3f7daf6c46456d247cdce4a
Ryancodes001 <ryancodes001@gmail.com>
2016-08-09 18:01:03 -0400

    adding user assign roles

---
+++ Bug_tracker/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Bug_tracker.Helpers;
+using Bug_tracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bug_tracker.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: AdminDashboard
+        public ActionResult AdminDashboard()
+        {
+            return View(db.Users.ToList());
+        }
+    
+
+
+    //GET Admin/EditUser
+
+    public ActionResult EditUser(string id)
+    {
+        var user = db.Users.Find(id);
+        AdminUserViewModel AdminModel = new AdminUserViewModel();
+        UserRolesHelper helper = new UserRolesHelper(db);
+        var selected = helper.ListUserRoles(id);
+        AdminModel.Roles = new MultiSelectList(db.Roles, "Name", "Name", selected);
+        AdminModel.User = user;
+        return View(AdminModel);
+     }
+   }
+}
+
+
